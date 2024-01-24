534759e96e9566f8db8f15382e45b01d725beb6b
Beka Abuladze <dbeqaabuladze@gmail.com>
2024-01-25 02:01:13 +0400

    modyfing js

---
+++ data.js
@@ -0,0 +1,39 @@
+const coursesData = [
+  {
+    imgSrc: "./assets/iosImg.webp",
+    title: "iOS Development",
+  },
+  {
+    imgSrc: "./assets/reactImg.webp",
+    title: "React",
+  },
+  {
+    imgSrc: "./assets/pythonImg.webp",
+    title: "Intro to Python",
+  },
+  {
+    imgSrc: "./assets/phthonAdvnc.webp",
+    title: "Advanced Python",
+  },
+  {
+    imgSrc: "./assets/cyberscr.jpg",
+    title: "Advanced Cybersecurity Course",
+  },
+  {
+    imgSrc: "./assets/tot.webp",
+    title: "ToT - Training of Trainers",
+  },
+  {
+    imgSrc: "./assets/blockch.webp",
+    title: "Blockchain",
+  },
+  {
+    imgSrc: "./assets/devops.webp",
+    title: "DevOps",
+  },
+  {
+    imgSrc: "./assets/infSecurity.webp",
+    title: "Information Security Governance",
+  },
+];
+export default coursesData;
--- main.js
+++ main.js
@@ -1,54 +1,76 @@
-const burgerMenu = document.querySelector(".hamburger");
+import coursesData from "./data.js";
+
+const burgerMenu = document.querySelector(".menu-icon");
 const burgerTopLine = document.querySelector(".hamburger-half");
 const burgerMiddleLine = document.querySelector(".hamburger-full");
 const burgerBottomLine = document.querySelector(".left");
 const burgerMenuList = document.querySelector(".burger-menu");
 const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
-const faq = document.querySelectorAll(".faq-quest");
+const faq = document.querySelectorAll(".faq-question");
 const faqArrow = document.querySelectorAll(".faq-arrow");
 const faqAnswer = document.querySelectorAll(".faq-answer");
+const header = document.querySelector(".main-header");
 
 burgerMenu.addEventListener("click", () => {
   burgerMiddleLine.classList.toggle("rotate");
   burgerTopLine.classList.toggle("rotate-top");
   burgerBottomLine.classList.toggle("rotate-bottom");
-  console.log("hey");
   burgerMenu.classList.toggle("transparant");
   burgerMenuList.classList.toggle("active");
   body.classList.toggle("no-scroll");
   overlay.style.opacity = overlay.style.opacity === "0.5" ? "0" : "0.5";
 });
-// slider
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("movingheader", scrollY > 1);
+});
+
 let slideImages = document.querySelectorAll(".slidediv");
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
 let dots = document.querySelectorAll(".dot");
 
-var counter = 0;
+const coursesList = document.getElementById("coursesList");
+
+coursesData.forEach((course, index) => {
+  const courseElement = document.createElement("div");
+  courseElement.classList.add("course");
+
+  courseElement.innerHTML = `
+    <img class="course__img" src="${course.imgSrc}" alt=" image of course" />
+    <div class="course__text">
+      <h5 class="course__headline">${course.title}</h5>
+      <p class="registration">რეგისტრაცია დასრულებულია</p>
+      <div>
+        <img src="./assets/arrow.svg" alt="arrow" />
+        <span class="course__details">კურსის დეტალები</span>
+      </div>
+    </div>
+  `;
+
+  coursesList.appendChild(courseElement);
+});
+
+let counter = 0;
+
 next.addEventListener("click", slideNext);
 function slideNext() {
   slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
-  if (counter >= slideImages.length - 1) {
-    counter = 0;
-  } else {
-    counter++;
-  }
+  counter = counter >= slideImages.length - 1 ? 0 : counter + 1;
   slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
   indicators();
 }
+
 prev.addEventListener("click", slidePrev);
 function slidePrev() {
   slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
-  if (counter == 0) {
-    counter = slideImages.length - 1;
-  } else {
-    counter--;
-  }
+  counter = counter === 0 ? slideImages.length - 1 : counter - 1;
   slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
   indicators();
 }
 
+let deletInterval;
 function autoSliding() {
   deletInterval = setInterval(timer, 6000);
   function timer() {
@@ -59,22 +81,19 @@ function autoSliding() {
 autoSliding();
 
 const container = document.querySelector(".slide-container");
-container.addEventListener("mouseover", function () {
-  clearInterval(deletInterval);
-});
 
+container.addEventListener("mouseover", () => clearInterval(deletInterval));
 container.addEventListener("mouseout", autoSliding);
 
 function indicators() {
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[counter].className += " active";
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === counter);
+  });
 }
 
 function switchImage(currentImage) {
   currentImage.classList.add("active");
-  var imageId = currentImage.getAttribute("attr");
+  const imageId = currentImage.getAttribute("attr");
   if (imageId > counter) {
     slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
     counter = imageId;
@@ -88,13 +107,10 @@ function switchImage(currentImage) {
   }
   indicators();
 }
-for (let i = 0; i < faq.length; i++) {
-  faq[i].addEventListener("click", () => {
-    for (let j = 0; j < faqAnswer.length; j++) {
-      if (j == i) {
-        faqAnswer[j].classList.toggle("closed");
-        faqArrow[j].classList.toggle("rotatearrow");
-      }
-    }
+
+faq.forEach((faqItem, i) => {
+  faqItem.addEventListener("click", () => {
+    faqAnswer[i].classList.toggle("closed");
+    faqArrow[i].classList.toggle("rotatearrow");
   });
-}
+});
